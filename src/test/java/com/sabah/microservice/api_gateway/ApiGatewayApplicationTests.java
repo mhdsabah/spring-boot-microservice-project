129c1d423f8e2b64c5cfe3deceec01e1e2bf98a0
Muhammed Sabah <muhammedsabah@icloud.com>
2025-03-22 22:10:24 +0530

    Added api-gateway service

---
+++ src/test/java/com/sabah/microservice/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabah.microservice.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
